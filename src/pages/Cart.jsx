84da3d6a67529ab7502cea506b661a7eba05962a
Babak Movahhedi <babak.movahhedi2015@gmail.com>
2022-06-13 16:58:06 +0430

    cartpage style edited

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,15 +1,38 @@
 import { useCart } from "../providers/CartProviders";
-
+import './cart.css';
 
 const Cart = () => {
     const cartState =useCart();
-    console.log(cartState.Cart);
-    return ( 
-       
-        <div>
-            <main>{cartState.Cart.length ? cartState.Cart.map((item)=>             
-             <div key={item.id}><p>{item.name}</p></div> )
-               : <p>No ITEm Add</p>}  </main>
+    if(!cartState.Cart)
+         return(<main>
+            <h2>cart is empty!</h2>
+         </main> )
+    return (        
+        <div className="cart_item_list ">
+           <section className="cart_item">
+            {cartState.Cart.map(
+                (item)=>
+                <div className="cart_item_product">
+
+                    <div className="cart_item_img">
+                    <img src={item.image}></img>
+                    </div>                   
+                   <div className="cart_item-title">
+                   <p>{item.name} </p>
+                    <p>{item.price * item.quantity} </p>
+                   </div>
+                    <div className="cart_btn">
+                    <button >Add</button>
+                    <button>number: {item.quantity} </button>
+                    <button>Remove</button>
+                   </div>
+                    
+                </div>
+            )}
+           </section>
+           <section className="cart_item_summery">
+            <h2>cart sumery</h2>
+           </section>
         </div>
      );
 }
