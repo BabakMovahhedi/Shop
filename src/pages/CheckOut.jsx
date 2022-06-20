5dfccbe594f3406fc5e676c039fc23946f775757
Babak Movahhedi <babak.movahhedi2015@gmail.com>
2022-06-20 22:34:32 +0430

    checkout page add

--- src/pages/CheckOut.jsx
+++ src/pages/CheckOut.jsx
@@ -1,11 +1,39 @@
-import { useParams } from "react-router-dom";
+import { useAouth } from "../providers/AouthProvider";
+import {useCart} from '../providers/CartProviders';
+import '../pages/checkout.css'
+
+
 
 
 const CheckOut = () => {
+    const Aouth=useAouth();
     
+    const {Cart}=useCart();
+    console.log(Cart);
    
     return ( 
-        <h2>CheckOut</h2>
+     <div className="checkout">
+    
+    <section className="section">
+          <h2>
+            <p>Name:{Aouth.name} </p>
+            <p>Email:{Aouth.email} </p>
+            <p>PhoneNumber:{Aouth.phoneNumber}</p>
+        </h2>
+      </section>
+    
+      
+      <section className="section">
+        {Cart.map((c)=>{
+            return (<div  key={c.id}>
+                <p>Name:{c.name} </p>
+                <p>price:{c.price} </p>
+                <p>OffPrice:{c.offPrice} </p>
+            </div>)
+        }) }    
+      </section>
+      
+     </div>
      );
 }
  
